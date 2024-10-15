c4886603118636f52dffa47e3a35d2bb536c2b57
242Ken_Lai <96279795+Kenlele@users.noreply.github.com>
2024-10-15 17:14:48 +0800

    Update key.js

--- key.js
+++ key.js
@@ -1,3 +1,3 @@
 //填上申請好的API KEY
-const subscriptionKey = "1be9e46347234c7e9838589a05a6ecf1";
-const subscriptionKey2 = "633a6f4d810143f3b02ed04e370ce5f6";
+const subscriptionKey = "1be9e46347234c7--------------";
+const subscriptionKey2 = "633a6f4d810143--------------";
